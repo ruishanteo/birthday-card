d939348ef481a29f83d7e02665de9a0d064a1214
ruishanteo <ruishan.teo03@gmail.com>
2023-04-22 11:39:14 +0800

    updated End page

--- src/Pages/End.js
+++ src/Pages/End.js
@@ -4,16 +4,27 @@ export function End() {
   return (
     <Box align="center" variant="text" height="705px">
       <Box height="200px"></Box>
-      <Button variant="contained">Yes</Button>
-      <Button
-        variant="contained"
-        onClick={() => {
-          alert("This is an error alert — check it out!");
-        }}
-      >
-        No
-      </Button>
-      <Typography mt={8}>Text</Typography>
+      <Typography variant="h3" mb={5} sx={{ fontFamily: "monospace" }}>
+        Text
+      </Typography>
+      <Box mt={5} justifyContent="center" display="flex" flexDirection="row">
+        <Button
+          variant="contained"
+          style={{ minHeight: "50px", minWidth: "150px" }}
+        >
+          Yes
+        </Button>
+        <Box width="50px"></Box>
+        <Button
+          variant="contained"
+          onClick={() => {
+            alert("This is an error alert — check it out!");
+          }}
+          style={{ minHeight: "50px", minWidth: "150px" }}
+        >
+          No
+        </Button>
+      </Box>
     </Box>
   );
 }
