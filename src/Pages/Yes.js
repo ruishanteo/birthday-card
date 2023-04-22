070990c1ec70890677e9143731bbf0125fd5a211
ruishanteo <ruishan.teo03@gmail.com>
2023-04-22 11:54:59 +0800

    updated Yes page

--- src/Pages/Yes.js
+++ src/Pages/Yes.js
@@ -4,8 +4,19 @@ export function Yes() {
   return (
     <Box align="center" variant="text" height="705px">
       <Box height="200px"></Box>
-      <Typography variant="h5">thank you for ...</Typography>
-      <Typography mt={8}>Text</Typography>
+      <Typography
+        variant="h5"
+        sx={{
+          minHeight: "50px",
+          minWidth: "150px",
+          fontFamily: "monospace",
+          fontWeight: "700",
+          fontSize: "80px",
+          letterSpacing: "5px",
+        }}
+      >
+        yay!
+      </Typography>
     </Box>
   );
 }
